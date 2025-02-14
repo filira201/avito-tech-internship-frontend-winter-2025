b3fe6bbfb56e38f4cf2a576e0fc04a038760a3e2
Egor <egor297198@mail.ru>
2025-02-14 14:35:19 +0400

    install react hook form and test validate

--- client/src/lib/schemes/schemes.ts
+++ client/src/lib/schemes/schemes.ts
@@ -1,30 +1,69 @@
 import { z } from "zod";
 
-const stringMin = (min: number, message: string) =>
-  z.string().trim().min(min, { message });
+const stringMinAndMax = (
+  min: number,
+  max: number,
+  messageMin: string,
+  messageMax: string
+) =>
+  z
+    .string()
+    .trim()
+    .min(min, { message: messageMin })
+    .max(max, { message: messageMax });
 
 const positiveNumber = (message: string) => z.number().positive({ message });
 
 export const BaseSchema = z.object({
-  name: stringMin(1, "Введите название объявления"),
-  description: stringMin(1, "Введите описание"),
-  location: stringMin(1, "Введите локацию"),
-  image: z.string().optional(),
+  name: stringMinAndMax(
+    1,
+    1000,
+    "Введите название объявления",
+    "Название объявления должно быть не длинее 1000 символов"
+  ),
+  description: stringMinAndMax(
+    1,
+    10000,
+    "Введите описание",
+    "Описание объявления должно быть не длинее 10000 символов"
+  ),
+  location: stringMinAndMax(
+    1,
+    1000,
+    "Введите локацию",
+    "Расположение локации должно быть не длинее 1000 символов"
+  ),
   type: z.enum(["Недвижимость", "Авто", "Услуги"], {
     errorMap: () => ({ message: "Выберите категорию" }),
   }),
+  image: z.string().optional(),
 });
 
 export const RealEstateSchema = BaseSchema.extend({
-  propertyType: stringMin(1, "Введите тип недвижимости"),
+  propertyType: stringMinAndMax(
+    1,
+    1000,
+    "Введите тип недвижимости",
+    "Тип недвижимости должен быть не длинее 1000 символов"
+  ),
   area: positiveNumber("Площадь должна быть положительным числом"),
   rooms: z.number().min(1, { message: "Минимум одна комната" }),
   price: positiveNumber("Цена должна быть положительным числом"),
 });
 
 export const AutoSchema = BaseSchema.extend({
-  brand: stringMin(1, "Введите марку автомобиля"),
-  model: stringMin(1, "Введите модель автомобиля"),
+  brand: stringMinAndMax(
+    1,
+    200,
+    "Введите марку автомобиля",
+    "Марка автомобиля должна быть не длинее 200 символов"
+  ),
+  model: stringMinAndMax(
+    1,
+    200,
+    "Введите модель автомобиля",
+    "Модель автомобиля должна быть не длинее 200 символов"
+  ),
   year: z
     .number()
     .max(new Date().getFullYear(), { message: "Некорректный год" }),
@@ -32,10 +71,15 @@ export const AutoSchema = BaseSchema.extend({
 });
 
 export const ServicesSchema = BaseSchema.extend({
-  serviceType: stringMin(1, "Введите тип услуги"),
+  serviceType: stringMinAndMax(
+    1,
+    1000,
+    "Введите тип услуги",
+    "Тип услуги должен быть не длинее 1000 символов"
+  ),
   experience: z
     .number()
     .min(0, { message: "Опыт не может быть отрицательным" }),
   cost: positiveNumber("Стоимость должна быть положительной"),
-  schedule: z.string().optional(),
+  workSchedule: z.string().optional(),
 });
--- client/src/routes/FormAdvertisement.tsx
+++ client/src/routes/FormAdvertisement.tsx
@@ -1,37 +1,127 @@
+import { useForm, SubmitHandler } from "react-hook-form";
 import { TypeAdvertisement } from "../models/types";
 import { advertisementApi } from "../services/AdvertisementService";
+import { BaseSchema } from "../lib/schemes/schemes";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type FormField = z.infer<typeof BaseSchema>;
 
 const FormAdvertisement = () => {
   const [createAdvertisement] =
     advertisementApi.useCreateAdvertisementMutation();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<FormField>({
+    defaultValues: { location: "Ижевск" },
+    resolver: zodResolver(BaseSchema),
+  });
 
-  const handleCreate = async () => {
-    const obj = {
-      name: "DSAKDKLASDKJLAKJDLAJKLDAKLJjkl",
-      description: "Просторная квартира в центре города",
-      location: "Москва",
-      type: "Недвижимость",
-      image:
-        "https://www.alfaplan.ru/upload/information_system_33/1/8/4/item_1846/information_items_property_254476.webp",
-      propertyType: "Квартира",
-      area: 100,
-      rooms: 3,
-      price: 15000000,
-    } as const;
-    await createAdvertisement(obj as TypeAdvertisement);
+  const onSubmit: SubmitHandler<FormField> = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      const response = await createAdvertisement(data as TypeAdvertisement);
+      if (response.error) {
+        throw new Error("Ошибка");
+      }
+    } catch (error) {
+      console.log(error);
+      setError("root", {
+        message: "SSSSS",
+      });
+    }
   };
 
   return (
-    <div>
-      <h2>Form</h2>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-4 mt-10"
+    >
+      <input
+        {...register("name")}
+        className="p-2 border border-zinc-600 rounded-lg"
+        type="text"
+        placeholder="name"
+      />
+      {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+      <input
+        {...register("description")}
+        className="p-2 border border-zinc-600 rounded-lg"
+        type="text"
+        placeholder="description"
+      />
+      {errors.description && (
+        <p className="text-red-500">{errors.description.message}</p>
+      )}
+      <input
+        {...register("location")}
+        className="p-2 border border-zinc-600 rounded-lg"
+        type="text"
+        placeholder="location"
+      />
+      {errors.location && (
+        <p className="text-red-500">{errors.location.message}</p>
+      )}
+      <input
+        {...register("type")}
+        className="p-2 border border-zinc-600 rounded-lg"
+        type="text"
+        placeholder="type"
+      />
+      {errors.type && <p className="text-red-500">{errors.type.message}</p>}
       <button
-        className="border border-violet-500 p-10 cursor-pointer"
-        onClick={handleCreate}
+        disabled={isSubmitting}
+        className={`${
+          isSubmitting ? "bg-amber-300" : "bg-green-500"
+        } border border-gray-200 p-4 rounded-lg`}
+        type="submit"
       >
-        Добавить новое объявление
+        {isSubmitting ? "Submitting..." : "Submit"}
       </button>
-    </div>
+      {errors.root && <p className="text-red-500">{errors.root.message}</p>}
+    </form>
   );
 };
 
 export default FormAdvertisement;
+
+// import { TypeAdvertisement } from "../models/types";
+// import { advertisementApi } from "../services/AdvertisementService";
+
+// const FormAdvertisement = () => {
+//   const [createAdvertisement] =
+//     advertisementApi.useCreateAdvertisementMutation();
+
+//   const handleCreate = async () => {
+//     const obj = {
+//       name: "DSAKDKLASDKJLAKJDLAJKLDAKLJjkl",
+//       description: "Просторная квартира в центре города",
+//       location: "Москва",
+//       type: "Недвижимость",
+//       image:
+//         "https://www.alfaplan.ru/upload/information_system_33/1/8/4/item_1846/information_items_property_254476.webp",
+//       propertyType: "Квартира",
+//       area: 100,
+//       rooms: 3,
+//       price: 15000000,
+//     } as const;
+//     await createAdvertisement(obj as TypeAdvertisement);
+//   };
+
+//   return (
+//     <div>
+//       <h2>Form</h2>
+//       <button
+//         className="border border-violet-500 p-10 cursor-pointer"
+//         onClick={handleCreate}
+//       >
+//         Добавить новое объявление
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default FormAdvertisement;
